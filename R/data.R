380e75be873d1b4540b835375679dec0cb7f2f3a
Ruyi Pan <panruyi91@gmail.com>
2025-07-10 18:52:46 -0400

    data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example Multimodal Data
+#'
+#' A simulated or real dataset for demonstrating the use of `Ceidr`.
+#'
+#' @format A list with components:
+#' \describe{
+#'   \item{m1}{A matrix (300 x 1000) for modality 1}
+#'   \item{m2}{A matrix (300 x 1000) for modality 2}
+#'   \item{distmat}{A 1000 x 1000 distance matrix}
+#'   \item{x}{A numeric vector of age}
+#'   \item{z}{A factor or numeric vector of sex}
+#' }
+#' @usage data(Data)
+#' @keywords datasets
+"Data"
